06dd7be4b366563e07797899290c4bd69109c5ce
Mike <kaliman.mike@gmail.com>
2026-06-20 11:46:57 -0400

    more comments, avoid root crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 // Root-level package allows us to enable the serde feature
-// on a workspace level withotu turning it on by default.
+// on a workspace level without turning it on by default.
 // Although it means we have this weird empty lib.rs file.
