ecbb5ce82d1aa8881aef95e388b1047556056c06
Mike <kaliman.mike@gmail.com>
2026-07-23 22:05:49 -0400

    use nextest

--- run_test.sh
+++ run_test.sh
@@ -6,5 +6,5 @@ export RUSTFLAGS="-D warnings"
 cargo check --workspace --all-targets --all-features
 cargo fmt
 cargo clippy --fix --workspace --all-targets --all-features --allow-dirty --allow-staged
-cargo test --workspace --all-features
-cargo +nightly miri test --workspace --all-features
+cargo nextest run --all-features
+cargo +nightly miri nextest run --all-features
