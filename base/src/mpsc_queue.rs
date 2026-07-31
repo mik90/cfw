18c7ec27b11789d608ff3b3cd160514071feb540
Mike <kaliman.mike@gmail.com>
2026-07-30 20:21:35 -0400

    initial transliteration of queue

--- base/src/mpsc_queue.rs
+++ base/src/mpsc_queue.rs
@@ -1,5 +1,13 @@
 use crossbeam_queue::ArrayQueue;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::env::temp_dir;
+use std::mem::MaybeUninit;
+use std::{
+    ops::{Deref, DerefMut},
+    sync::atomic::{
+        AtomicBool, AtomicU8, AtomicUsize,
+        Ordering::{self, Acquire, Relaxed, Release},
+    },
+};
 
 /// A bounded, lock-free MPSC queue backed by `crossbeam_queue::ArrayQueue`.
 /// When the queue is full, `push` displaces the oldest element (front) to make room.
@@ -54,43 +62,141 @@ impl<T> MpscQueue<T> {
     }
 }
 
+// Default to 64, could differ per platform
+#[repr(align(64))]
+#[derive(Debug)]
+struct CacheAligned<T> {
+    value: T,
+}
+
+impl<T> CacheAligned<T> {
+    pub fn new(value: T) -> Self {
+        CacheAligned { value }
+    }
+}
+
+impl<T> Deref for CacheAligned<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T> DerefMut for CacheAligned<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
+
+impl<T> From<T> for CacheAligned<T> {
+    fn from(value: T) -> Self {
+        CacheAligned { value }
+    }
+}
+
+struct QueueSlot<T> {
+    pub turn: CacheAligned<AtomicU8>,
+    pub full: CacheAligned<AtomicBool>,
+    pub value: MaybeUninit<T>,
+}
+impl<T> QueueSlot<T> {
+    pub fn new(value: T) -> Self {
+        QueueSlot {
+            turn: AtomicU8::new(0).into(),
+            full: AtomicBool::new(false).into(),
+            value: MaybeUninit::<T>::new(value),
+        }
+    }
+}
+
 ///  https://blog.bearcats.nl/simple-message-queue/ (modified rigtorp?) to replace cross-beam channel usage which is MPMC
 pub struct MpscQueue2<T> {
     /// Cumulative count of elements displaced by `push` due to overflow.
-    dropped: AtomicUsize,
-    marker: std::marker::PhantomData<T>,
-    /// TODO Align by cache width similar to cache_padded.rs in crossbeam
-    write_ticket: AtomicUsize,
-    read_ticket: usize,
+    dropped: CacheAligned<AtomicUsize>,
+    write_ticket: CacheAligned<AtomicUsize>,
+    /// Read ticket doesn't need to be atomic, there's a single reader
+    read_ticket: CacheAligned<usize>,
     capacity: usize,
+    slots: Vec<QueueSlot<T>>,
 }
 
 impl<T> MpscQueue2<T> {
-    pub fn new(_capacity: usize) -> Self {
-        todo!()
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            dropped: AtomicUsize::new(0).into(),
+            write_ticket: AtomicUsize::new(0).into(),
+            read_ticket: 0.into(),
+            capacity,
+            slots: Vec::with_capacity(capacity),
+        }
     }
 
-    pub fn push(&self, _value: T) -> bool {
-        todo!()
+    pub fn push(&mut self, value: T) -> bool {
+        let new_write_ticket = self.write_ticket.fetch_add(1, Relaxed);
+        let turn = (new_write_ticket / self.capacity) as u8;
+
+        let cur_slot = &mut self.slots[new_write_ticket % self.capacity];
+        // Check if it's not our turn and if we should drop
+        // serialize with reader
+        if turn != cur_slot.turn.load(Acquire) {
+            self.dropped.fetch_add(1, Relaxed);
+            return false;
+        }
+
+        cur_slot.full.store(true, Release); // serialize with reader
+        cur_slot.value = MaybeUninit::new(value);
+        true
     }
 
     pub fn dropped(&self) -> usize {
-        todo!()
+        self.dropped.load(Relaxed)
     }
 
-    pub fn pop(&self) -> Option<T> {
-        todo!()
+    pub fn pop(&mut self) -> Option<T> {
+        let read_ticket = *self.read_ticket;
+        // TODO could just make read ticket atomic even though i dont have a way to
+        // avoid exposing reader interface to multiple threads
+        *self.read_ticket += 1;
+
+        let turn = (read_ticket / self.capacity) as u8;
+
+        let cur_slot = &mut self.slots[read_ticket % self.capacity];
+        if !cur_slot.full.load(Acquire) {
+            // nothing left
+            return None;
+        }
+        let mut return_value = MaybeUninit::uninit();
+
+        // SAFETY: Both the cur slot and return value are the same type/alignment
+        unsafe {
+            // The return value now has the data from the current slot.
+            // So the current slot is uninitialized memory.
+            std::ptr::swap(cur_slot.value.as_mut_ptr(), return_value.as_mut_ptr());
+        }
+
+        cur_slot.full.store(false, Release);
+        cur_slot.turn.store(turn + 1, Release); // serialize w/ writer
+        // SAFETY: We initialize values in push(), and we just swapped with an initialized value
+        unsafe { Some(return_value.assume_init()) }
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!()
+        self.slots
+            .iter()
+            .all(|slot| slot.full.load(Relaxed) == false)
     }
 
-    pub fn clear(&self) {
-        todo!()
+    pub fn clear(&mut self) {
+        while let Some(_) = self.pop() {}
     }
 
     pub fn len(&self) -> usize {
-        todo!()
+        let mut count = 0;
+        for entry in self.slots.iter() {
+            if entry.full.load(Relaxed) {
+                count += 1;
+            }
+        }
+        count
     }
 }
