48e0eb6920c509b57b2e5da088f017a639f9dc93
Mike <kaliman.mike@gmail.com>
2026-08-11 19:49:37 -0400

    more callbacks and frequent publishes

--- run_flamegraph.sh
+++ run_flamegraph.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 OUTPUT_DIR="$PWD/tmp"
 STARTUP_DELAY_MS=3000
-DURATION_SECS=15
 PERF_SAMPLE_PER_SEC=997
 STACK_DUMP_SIZE_BYTES=64000
 
@@ -20,7 +19,7 @@ RUSTFLAGS="-Clink-arg=-Wl,--no-rosegment" \
     cargo flamegraph --bin=$PROFILE_TARGET --profile profiling \
      -c "$PERF_COMMAND" \
      -o "$OUTPUT_DIR/flamegraph.svg" \
-     -- --duration-secs $DURATION_SECS
+     -- --duration-secs 15 --sets 8 --period-us 1
 
 pushd $OUTPUT_DIR
 perf script report gecko --save-only perf_gecko.json
