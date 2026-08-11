2dd1b53c8e5d05cf26346b1854f3481b6d0df947
Mike <kaliman.mike@gmail.com>
2026-08-11 19:16:29 -0400

    re-org script

--- run_flamegraph.sh
+++ run_flamegraph.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PROFILE_TARGET="profiling"
 OUTPUT_DIR="$PWD/tmp"
 STARTUP_DELAY_MS=3000
 DURATION_SECS=15
+PERF_SAMPLE_PER_SEC=997
+STACK_DUMP_SIZE_BYTES=64000
+
+PERF_COMMAND="record -D $STARTUP_DELAY_MS \
+    --freq=$PERF_SAMPLE_PER_SEC \
+    --call-graph dwarf,$STACK_DUMP_SIZE_BYTES \
+    -o $OUTPUT_DIR/perf.data"
 
 mkdir -p $OUTPUT_DIR
 
+PROFILE_TARGET="profiling"
+
 RUSTFLAGS="-Clink-arg=-Wl,--no-rosegment" \
     cargo flamegraph --bin=$PROFILE_TARGET --profile profiling \
-     -c "record -D $STARTUP_DELAY_MS -F 997 --call-graph dwarf,64000 -g -o $OUTPUT_DIR/perf.data" \
-     -o $OUTPUT_DIR/flamegraph.svg \
+     -c "$PERF_COMMAND" \
+     -o "$OUTPUT_DIR/flamegraph.svg" \
      -- --duration-secs $DURATION_SECS
 
 pushd $OUTPUT_DIR
