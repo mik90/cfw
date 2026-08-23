9a388a67c41b534bce4682fc89dfcd2e6c3d4491
Mike <kaliman.mike@gmail.com>
2026-08-23 10:19:33 -0400

    switch to bidirectional map

--- task/src/string_interner.rs
+++ task/src/string_interner.rs
@@ -1,110 +1,95 @@
+use std::cmp::{Eq, PartialEq};
 use std::collections::HashMap;
-use std::mem;
-use std::sync::{Arc, RwLock};
+use std::hash::Hash;
+use std::marker::PhantomData;
 
-/// Allows for interning strings such as channel names and callback names
-/// Copied from https://matklad.github.io/2020/03/22/fast-simple-rust-interner.html
-pub struct StringInterner {
-    /// 'static is used to fake interior references
-    string_to_hash: HashMap<&'static str, InternId>,
-    /// References to interned strings
-    interned_strings: Vec<&'static str>,
-    /// Other containers point to this buffer
-    contiguous_storage: String,
-    /// Old storage containeres that weren't big enough
-    overflow_storage: Vec<String>,
+/// Strong-type for accessing interned channel names or task names
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct InternId<MarkerType> {
+    id: u32,
+    _marker: PhantomData<MarkerType>,
 }
 
-impl StringInterner {
-    pub fn with_capacity(char_capacity: usize) -> StringInterner {
-        let cap = char_capacity.next_power_of_two();
+impl<MarkerType> Clone for InternId<MarkerType> {
+    fn clone(&self) -> Self {
+        Self {
+            id: self.id,
+            _marker: Default::default(),
+        }
+    }
+}
+
+impl<MarkerType> Copy for InternId<MarkerType> {}
+
+impl<MarkerType> InternId<MarkerType> {
+    fn new(id: u32) -> InternId<MarkerType> {
+        InternId {
+            id,
+            _marker: Default::default(),
+        }
+    }
+}
+
+impl<MarkerType> PartialEq for InternId<MarkerType> {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl<MarkerType> Eq for InternId<MarkerType> {}
+
+impl<MarkerType> Hash for InternId<MarkerType> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+/// Bidirectional map for looking up data given ID, or ID given data
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct StringInterner<InternType> {
+    id_to_data: HashMap<InternId<InternType>, String>,
+    data_to_id: HashMap<String, InternId<InternType>>,
+}
+
+impl<InternType> StringInterner<InternType> {
+    pub fn with_capacity(capacity: usize) -> Self {
         StringInterner {
-            string_to_hash: HashMap::default(),
-            interned_strings: Vec::new(),
-            contiguous_storage: String::with_capacity(cap),
-            overflow_storage: Vec::new(),
+            id_to_data: HashMap::with_capacity(capacity),
+            data_to_id: HashMap::with_capacity(capacity),
         }
     }
 
-    pub fn intern(&mut self, value: &str) -> InternId {
-        if let Some(&intern_id) = self.string_to_hash.get(value) {
+    pub fn intern(&mut self, value: &str) -> InternId<InternType> {
+        if let Some(&intern_id) = self.data_to_id.get(value) {
             return intern_id;
         }
-        // SAFETY: Strings are kept alive by our invariants when used internally
-        let name = unsafe { self.alloc(value) };
         // IDs are just the entry in the map. u32 max strings would be a _huge_ amount,
         // so u32 is fine.
-        let id = InternId::new(self.string_to_hash.len() as u32);
-        self.string_to_hash.insert(name, id);
-        self.interned_strings.push(name);
+        let id = InternId::new(self.data_to_id.len() as u32);
 
-        debug_assert!(self.lookup(id) == name);
-        debug_assert!(self.intern(name) == id);
+        self.id_to_data.insert(id, value.into());
+        self.data_to_id.insert(value.into(), id);
 
         id
     }
 
-    pub fn lookup(&self, intern_id: InternId) -> &str {
-        self.interned_strings[intern_id.id as usize]
+    pub fn lookup_by_id(&self, intern_id: InternId<InternType>) -> &str {
+        self.id_to_data
+            .get(&intern_id)
+            .expect("InternIds should guarantee that values are always present")
     }
 
-    unsafe fn alloc(&mut self, name: &str) -> &'static str {
-        let cap = self.contiguous_storage.capacity();
-        if cap < self.contiguous_storage.len() + name.len() {
-            // Not enough space for this new string
-            let new_cap = (cap.max(name.len()) + 1).next_power_of_two();
-            let new_buf = String::with_capacity(new_cap);
-            // Replace our current storage with the new buf
-            let old_buf = mem::replace(&mut self.contiguous_storage, new_buf);
-            // Add our old storage to the overflow
-            self.overflow_storage.push(old_buf);
-        }
-
-        let interned = {
-            let start = self.contiguous_storage.len();
-            self.contiguous_storage.push_str(name);
-            &self.contiguous_storage[start..]
-        };
-
-        // SAFETY: We ensure that all interned strings stay alive in our main/overflow buffers
-        unsafe { &*(interned as *const str) }
-    }
-}
-
-/// Strong-type for accessing interned strings
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-pub struct InternId {
-    id: u32,
-}
-
-impl InternId {
-    fn new(id: u32) -> InternId {
-        InternId { id }
-    }
-}
-
-pub struct SharedStringInterner {
-    interner: Arc<RwLock<StringInterner>>,
-}
-
-impl SharedStringInterner {
-    pub fn with_capacity(char_capacity: usize) -> SharedStringInterner {
-        SharedStringInterner {
-            interner: Arc::new(RwLock::new(StringInterner::with_capacity(char_capacity))),
-        }
+    /// May not return an ID since we're using possibly un-interned strings
+    pub fn lookup_by_value(&self, value: &str) -> Option<InternId<InternType>> {
+        self.data_to_id.get(value).copied()
     }
 
-    pub fn intern(&self, value: &str) -> InternId {
-        self.interner
-            .write()
-            .expect("Interner RwLock is poisioned")
-            .intern(value)
-    }
-
-    pub fn lookup(&self, intern_id: InternId, f: impl FnOnce(&str)) {
-        let interner_guard = self.interner.read().expect("Interner RwLock is poisioned");
-        let value = interner_guard.lookup(intern_id);
-        f(value);
+    /// Minimizes size before passing to users, should not increase in size since users willlll
+    /// get this as immutable
+    pub fn shrink_to_fit(&mut self) {
+        self.id_to_data.shrink_to_fit();
+        self.data_to_id.shrink_to_fit();
     }
 }
 
@@ -112,41 +97,22 @@ impl SharedStringInterner {
 mod tests {
     use crate::string_interner::StringInterner;
 
+    #[derive(Debug)]
+    struct DummyMarker {}
+    type TestInterner = StringInterner<DummyMarker>;
+
     #[test]
     fn test_intern() {
-        let mut interner = StringInterner::with_capacity(10);
+        let mut interner = TestInterner::with_capacity(10);
         let interned_hello = interner.intern("hello");
-
-        let lookup_result = interner.lookup(interned_hello);
-
-        assert_eq!(lookup_result, "hello");
-    }
-
-    #[test]
-    fn test_over_capacity() {
-        let hello_string = "hello";
-        let mut interner = StringInterner::with_capacity(hello_string.len());
-        let interned_hello = interner.intern(hello_string);
-        assert_eq!(interner.overflow_storage.len(), 0);
         let interned_world = interner.intern("world");
-        assert_eq!(interner.overflow_storage.len(), 1);
+        interner.shrink_to_fit();
 
-        let lookup_hello = interner.lookup(interned_hello);
-        assert_eq!(lookup_hello, "hello");
-        let lookup_world = interner.lookup(interned_world);
-        assert_eq!(lookup_world, "world");
-    }
+        assert_eq!(interner.lookup_by_id(interned_hello), "hello");
+        assert_eq!(interner.lookup_by_id(interned_world), "world");
 
-    #[test]
-    fn test_many_interns() {
-        let mut interner = StringInterner::with_capacity(100);
-        let mut intern_ids = vec![];
-        for i in 0..100 {
-            intern_ids.push(interner.intern(&i.to_string()));
-        }
-
-        for (i, intern_id) in intern_ids.iter().enumerate() {
-            assert_eq!(interner.lookup(*intern_id), i.to_string());
-        }
+        assert_eq!(interner.lookup_by_value("hello").unwrap(), interned_hello);
+        assert_eq!(interner.lookup_by_value("world").unwrap(), interned_world);
+        assert!(interner.lookup_by_value("not-there").is_none());
     }
 }
