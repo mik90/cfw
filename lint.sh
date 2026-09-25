8417b10d5248c944af11f807b55c4e7f1524445b
Mike <kaliman.mike@gmail.com>
2026-09-24 20:52:01 -0400

    remove old test scripts

--- lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-export RUSTFLAGS="-D warnings"
-
-cargo check --workspace --all-targets --all-features
-cargo fmt
-cargo clippy --fix --workspace --all-targets --all-features --allow-dirty --allow-staged
